9e321db168d4100e2c060f9aa2f9348c77920daa
Manuel <mal8@alu.uma.es>
2018-05-23 18:38:52 +0200

    Corrección a la clase Imprimir.java
    Errores derivados de los cambios en Genes.java

--- src/Robot/Imprimir.java
+++ src/Robot/Imprimir.java
@@ -23,13 +23,13 @@ public class Imprimir {
 		
 		sc2.close();
 		sc2 = new Scanner(sc.nextLine());
-		genio.rangeOfSpeeds(sc2.nextDouble(), sc2.nextDouble());
+		genio.rangeOfSpeeds(sc2.nextInt(), sc2.nextInt());
 		
 		sc2.close();
 		sc2 = new Scanner(sc.nextLine());
 		genio.minimumSpeed(sc2.nextDouble(), sc2.nextDouble());
 		
-		genio.crearCromosoma();
+		genio.crearGenotipo();
 		
 		sc2.close();
 		sc.close();
@@ -40,10 +40,10 @@ public class Imprimir {
 
     public void printGenes(String filename, Genes genes) throws FileNotFoundException {
     	PrintWriter printGenes = new PrintWriter(new File(filename));
-    	printGenes.println(genes.getJengibreDistance().getAllele());
-    	printGenes.println(genes.getJengibreSpeedProbability().getAllele());
-    	printGenes.println(genes.getJengibreSpeedRange().getAllele());
-    	printGenes.println(genes.getJengibreLento().getAllele());
+    	printGenes.println(genes.getJengibreDistance());
+    	printGenes.println(genes.getJengibreSpeedProbability());
+    	printGenes.println( genes.getJengibreSpeedRange());
+    	printGenes.println(genes.getJengibreLento());
     	
     	
     }
