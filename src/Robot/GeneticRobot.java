a0ce1d9d4341f71a3d2e1b56a5a447584c5abfe0
Manuel <mal8@alu.uma.es>
2018-05-24 11:06:36 +0200

    Auto stash before merge of "master" and "origin/master"
    Inclusión del método printGenes

--- src/Robot/GeneticRobot.java
+++ src/Robot/GeneticRobot.java
@@ -1,9 +1,28 @@
 package Robot;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 import robocode.Robot;
 
 public interface GeneticRobot {
-    void run();
+	void run();
 
+	Genes getCurrentGenes(String filename);
+
+<<<<<<< Updated upstream
     Genes getCurrentGenes(String filename);
+=======
+	static void printGenes(String filename, Genes genes) throws FileNotFoundException {
+		PrintWriter printGenes = new PrintWriter(new File(filename));
+		printGenes.println(genes.getJengibreDistance());
+		printGenes.println(genes.getJengibreSpeedProbability());
+		printGenes.println( genes.getJengibreSpeedRange());
+		printGenes.println(genes.getJengibreLento());
+
+	}
+
+	Genes applyFitnessFunction(double lifeLeft, int victories, double timeSpent);
+>>>>>>> Stashed changes
 }
